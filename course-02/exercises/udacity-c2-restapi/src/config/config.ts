e7d0dfe7b136cb99920587ae44ca3b8f1fdaa0ea
ritvikkolhe <kolheritvik21@gmail.com>
2021-08-13 18:38:13 -0400

    RestAPI update

--- course-02/exercises/udacity-c2-restapi/src/config/config.ts
+++ course-02/exercises/udacity-c2-restapi/src/config/config.ts
@@ -5,9 +5,9 @@ export const config = {
     "database": "labdataritvik",
     "host": "labdataritvik.cjfeeqm91o1i.us-east-1.rds.amazonaws.com",
     "dialect": "postgres",
-    "aws_region": "us-east-2",
-    "aws_profile": "default",
-    "aws_media_bucket": "udagram-ruttner-dev"
+    "aws_region": "us-east-1",
+    "aws_profile": "DEPLOYED",
+    "aws_media_bucket": "labdataritvikdev"
   },
   "prod": {
     "username": "",
